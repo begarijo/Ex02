59327fb1a3c9e590e3948c9db068eaf86a836799
Belén Garijo <117202411+begarijo@users.noreply.github.com>
2023-03-27 16:55:55 +0200

    Update ft_strcspn.c

--- Level2/ft_strcspn.c
+++ Level2/ft_strcspn.c
@@ -17,10 +17,10 @@ size_t	ft_strcspn(const char *s, const char *reject)
 	int	i;
 	int	j;
 
-	j = 0;
 	i = 0;
 	while (s[i] != '\0'))
 	{
+		j = 0;
 		while (reject[j] != '\0')
 		{
 			if (s[i] == reject[j])
